dec2f5d90afccdfb2892828a75923df1a28ba8b8
Vladislav Ustinov <vladislav140203@mail.ru>
2024-05-02 17:44:29 +0700

    Initial commit

--- NoteAppUI/Form1.Designer.cs
+++ NoteAppUI/Form1.Designer.cs
@@ -110,17 +110,17 @@
             this.label1.Location = new System.Drawing.Point(17, 34);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.Size = new System.Drawing.Size(147, 16);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Show category:";
+            this.label1.Text = "Показать категории :";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(133, 31);
+            this.comboBox1.Location = new System.Drawing.Point(163, 31);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(248, 24);
+            this.comboBox1.Size = new System.Drawing.Size(218, 24);
             this.comboBox1.TabIndex = 2;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -165,7 +165,7 @@
             // dateTimePickerModified
             // 
             this.dateTimePickerModified.Enabled = false;
-            this.dateTimePickerModified.Location = new System.Drawing.Point(425, 81);
+            this.dateTimePickerModified.Location = new System.Drawing.Point(393, 81);
             this.dateTimePickerModified.Margin = new System.Windows.Forms.Padding(4);
             this.dateTimePickerModified.Name = "dateTimePickerModified";
             this.dateTimePickerModified.Size = new System.Drawing.Size(265, 22);
@@ -174,12 +174,12 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(349, 85);
+            this.label4.Location = new System.Drawing.Point(317, 85);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.Size = new System.Drawing.Size(79, 16);
             this.label4.TabIndex = 5;
-            this.label4.Text = "Modified:";
+            this.label4.Text = "Обновлен :";
             // 
             // dateTimePickerCreated
             // 
@@ -196,9 +196,9 @@
             this.label3.Location = new System.Drawing.Point(24, 85);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.Size = new System.Drawing.Size(62, 16);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Created:";
+            this.label3.Text = "Создан :";
             // 
             // labelCategory
             // 
@@ -216,9 +216,9 @@
             this.label2.Location = new System.Drawing.Point(24, 55);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.Size = new System.Drawing.Size(81, 16);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Category:";
+            this.label2.Text = "Категория :";
             // 
             // Header
             // 
@@ -227,9 +227,9 @@
             this.Header.Location = new System.Drawing.Point(20, 16);
             this.Header.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Header.Name = "Header";
-            this.Header.Size = new System.Drawing.Size(93, 29);
+            this.Header.Size = new System.Drawing.Size(229, 29);
             this.Header.TabIndex = 0;
-            this.Header.Text = "Header";
+            this.Header.Text = "Название заметки";
             this.Header.Click += new System.EventHandler(this.Header_Click);
             // 
             // buttonAdd
